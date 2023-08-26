79a73d90e3ad7a0652578a26399fac59621773b4
Ssingha46 <141923328+Ssingha46@users.noreply.github.com>
2023-08-26 16:29:38 -0700

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -15,7 +15,7 @@ class mr(object):
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
 ├🌀 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://www.oracle.com/cloud/>Render</a>
-├📊 BUILD-STATUS : Python v3.10.5              
+├📊 BUILD-STATUS : Python Latest              
 ╰───────────────⍟
                                 """
     HELP_TXT = """
